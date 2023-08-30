b262a4307c96480d5210a0ccd0b853ea16c12a38
Gurmeet Kaur <131999481+kgurmeet@users.noreply.github.com>
2023-08-30 10:20:40 +0530

    Create pattern8.java

---
+++ pattern8.java
@@ -0,0 +1,15 @@
+
+public class pattern8 {
+    public static void main(String[] args) {
+        int k=6,y=6;
+        for(k=6;k>=1;k--)
+        {
+            for(y=6;y>=k;y--)
+            {
+                System.out.print(k);
+            }
+            System.out.println();
+        }
+    }
+    
+}
